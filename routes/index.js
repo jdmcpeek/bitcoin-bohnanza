c3d8fd18dd63535623b6ce5fde5f86d4e0476a3b
jdmcpeek <david.mcpeek@yale.edu>
2014-11-15 03:02:56 -0500

    add play function

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'BitCoin Bohnanza' });
 });
 
+/* GET game page */
+router.get('/play', function(req, res) {
+	res.render('play', { title: "Play Bohnanza" });
+});
+
 module.exports = router;
