dcb7c0008b5ea9b97ac72f3b5fa23fea35adb6c2
jdmcpeek <david.mcpeek@yale.edu>
2014-11-17 02:09:25 -0500

    attach bean constructor fns to beans object, which will be exported

--- beans.js
+++ beans.js
@@ -1,30 +1,77 @@
-module.exports = {
-	function Bean(name, total, num1, num2, num3, num4, owner="deck") {
-		this.name = name;
-		this.total = total;
-		this.num1 = num1;
-		this.num2 = num2;
-		this.num3 = num3;
-		this.num4 = num4;
-		this.owner = owner
-	};
+var beans = {};
 
-	var coffee = new Bean("coffee bean", 24, 4, 7, 10, 12);
-	var wax = new Bean("wax bean", 22, 4, 7, 9, 11);
-	var blue = new Bean("blue bean", 20, 4, 6, 8, 10);
-	var chili = new Bean("chili bean", 18, 3, 6, 8, 9);
-	var stink = new Bean("stink bean", 16, 3, 5, 7, 8);
-	var green = new Bean("green bean", 14, 3, 5, 6, 7);
-	var soy = new Bean("soy bean", 12, 2, 4, 6, 7);
-	var blackeyed = new Bean("blackeyed bean", 10, 2, 4, 5, 6);
-	var red = new Bean("red bean", 8, 2, 3, 4, 5);
-	var garden = new Bean("garden bean", 1000, 2, 3, 1000);
-	var cocoa = new Bean("cocoa bean", 1000, 2, 3, 4);
+beans.protobean = function(name, total, num1, num2, num3, num4, owner) {
+	this.name = name;
+	this.total = total;
+	this.num1 = num1;
+	this.num2 = num2;
+	this.num3 = num3;
+	this.num4 = num4;
+	this.owner = (typeof(owner) === "undefined") ? "deck" : owner;
+};
+
+beans.coffee = function(card) {
+	var card = new this.bean("coffee bean", 24, 4, 7, 10, 12);
+	return card;
+};
+
+console.log(beans.coffee("coffee1"));
 
 
-
-
-
-
-
+beans.wax = function(card) {
+	var card = new Bean("wax bean", 22, 4, 7, 9, 11);
+	return card;
 }
+
+beans.blue = function(card) {
+	var card = new Bean("blue bean", 20, 4, 6, 8, 10);
+	return card;
+}
+
+beans.chili = function(card) {
+	var card = new Bean("chili bean", 18, 3, 6, 8, 9);
+	return card;
+}
+
+beans.stink = function(card) {
+	var card = new Bean("stink bean", 16, 3, 5, 7, 8);
+	return card;
+}
+
+beans.green = function(card) {
+	var card = new Bean("green bean", 14, 3, 5, 6, 7);
+	return card;
+}
+
+beans.soy = function(card) {
+	var card = new Bean("soy bean", 12, 2, 4, 6, 7);
+	return card;
+}
+
+beans.blackeyed = function(card) {
+	var card = new Bean("blackeyed bean", 10, 2, 4, 5, 6);
+	return card;
+}
+
+beans.red = function(card) {
+	var card = new Bean("red bean", 8, 2, 3, 4, 5);
+}
+
+beans.garden = function(card) {
+	var card = new Bean("garden bean", 1000, 2, 3, 1000);
+	return card;
+}
+
+beans.cocoa = function(card) {
+	var card = new Bean("cocoa bean", 1000, 2, 3, 4);
+	return card;
+}
+
+	
+
+	// function Deck()
+
+module.exports = beans;
+
+
+
