aafdbcc751203b81f3a5d3411baa9d12893a670f
jdmcpeek <david.mcpeek@yale.edu>
2014-11-18 01:19:48 -0500

    hidden property is messing up my function call

--- deck.js
+++ deck.js
@@ -4,38 +4,41 @@ var beans = require('./beans');
 
 var deck = {};
 
-deck.coffee = {};
-deck.wax = {};
-deck.blue = {};
-deck.chili = {};
-deck.stink = {};
-deck.green = {};
-deck.soy = {};
-deck.blackeyed = {};
-deck.red = {};
-deck.garden = {};
-deck.cocoa = {};
+deck.coffee = { "total": 24, "cards": [] };
+deck.wax = { "total": 22, "cards": [] };
+deck.blue = { "total": 20, "cards": [] };
+deck.chili = { "total": 18, "cards": [] };
+deck.stink = { "total": 16, "cards": [] };
+deck.green = { "total": 14, "cards": [] };
+deck.soy = { "total": 12, "cards": [] };
+deck.blackeyed = { "total": 10, "cards": [] };
+deck.red = { "total": 8, "cards": [] };
+deck.garden = { "total": 6, "cards": [] };
+deck.cocoa = { "total": 4, "cards": [] };
 
 
 
-deck.makedeck = function(coffee, wax, blue, chili, stink, green, soy, blackeyed, red, garden, cocoa) {
+deck.makedeck = function() {
 
+	for (var bohn in this) {
 
-	for (var bean in deck) {
+			// for (var i = 0; i < bohn.total; i++) {
+			// 	this[bohn][cards].push("beans[bohn](bohn)");
+			// }
 
-		for (var i = 0; i < coffee; i++) {
-			deck.coffee[i] = beans.coffee("coffee");
-		}
+			if (this.hasOwnProperty(bohn)) {
+
+				// this[bohn]["cards"].push("fun");
+				// console.log(this[bohn]["cards"]);
+
+				console.log(this[bohn]);
+
+			}
 
 	}
-
-
-
-
+	// return this;
 };
 
-
-console.log(deck.makedeck(7));
-
+console.log(deck.makedeck());
 
 module.exports = deck;
