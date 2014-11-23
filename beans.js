2a9ae5d781d69ebfc4924bb70f13c11824c5ad73
jdmcpeek <david.mcpeek@yale.edu>
2014-11-23 14:50:43 -0500

    problem: process.exit() fucks with program flow; exits process before data can be saved through callback

--- beans.js
+++ beans.js
@@ -4,47 +4,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-var Schema = mongoose.Schema;
 
-var beanSchema = new Schema({
-	type: String,
-	owner: String,
-	gold_I: Number,
-	gold_II: Number,
-	gold_III: Number,
-	gold_IV: Number
-});
-
-var Bean = mongoose.model('Bean', beanSchema);
-
-var wax = new Bean({
-	"type": "wax",
-	"owner": "deck",
-	"gold_I": 4,
-	"gold_II": 7,
-	"gold_III": 9,
-	"gold_IV": 11
-});
-
-
-db.beans.save(wax);
-
-// wax.save(function (err, wax) {
-// 	if (err) return console.error(err);
-// 	console.log(wax.type);
-// });
-
-
-});
-
-process.exit();
-
-
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-	console.log('opened db successfully');
 	var Schema = mongoose.Schema;
 
 	var beanSchema = new Schema({
@@ -70,151 +30,190 @@ db.once('open', function callback () {
 
 	wax.save(function (err, wax) {
 		if (err) return console.error(err);
-		console.log(coffee.type);
+		console.log(wax.type);
+		console.log('save, bitches!');
 	});
 
-
-
-
-
-
-
 });
 
-// var search = db.beans.find();
-// console.log(db);
-
-// console.log(search);
 
 
-process.exit();
 
 
-var beans = {};
 
-beans.beanSchema = new Schema({
-	type: String,
-	owner: String,
-	gold_I: Number,
-	gold_II: Number,
-	gold_III: Number,
-	gold_IV: Number
-});
-
-beans.protobean = mongoose.model('Bean', beans.beanSchema);
-
-
-beans.coffee = {
-	"type": "coffee",
-	"owner": "deck",
-	"gold_I": 4,
-	"gold_II": 7,
-	"gold_III": 10,
-	"gold_IV": 12
-};
-
-var err = "failed to save";
-var coffee = new beans.protobean(beans.coffee);
-coffee.save(function (err, coffee) {
-	if (err) return console.error(err);
-	console.log(coffee._id());
-});
-
-console.log("test");
-
-// for (var i; i < 5; i++) {
-// 	new beans.protobean(beans.coffee);
-// }
-// console.log(coffee);
-
-beans.wax = {
-	"type": "wax",
-	"owner": "deck",
-	"gold_I": 4,
-	"gold_II": 7,
-	"gold_III": 9,
-	"gold_IV": 11
-};
-
-beans.blue = {
-	"type": "blue",
-	"owner": "deck",
-	"gold_I": 4,
-	"gold_II": 6,
-	"gold_III": 8,
-	"gold_IV": 10
-};
-
-beans.chili = {
-	"type": "chili",
-	"owner": "deck",
-	"gold_I": 3,
-	"gold_II": 6,
-	"gold_III": 8,
-	"gold_IV": 9
-};
-
-beans.stink = {
-	"type": "stink",
-	"owner": "deck",
-	"gold_I": 3,
-	"gold_II": 5,
-	"gold_III": 7,
-	"gold_IV": 8
-};
-
-beans.green = {
-	"type": "green",
-	"owner": "deck",
-	"gold_I": 3,
-	"gold_II": 5,
-	"gold_III": 6,
-	"gold_IV": 7
-};
-
-beans.soy = {
-	"type": "soy",
-	"owner": "deck",
-	"gold_I": 2,
-	"gold_II": 4,
-	"gold_III": 6,
-	"gold_IV": 7
-};
-
-beans.blackeyed = {
-	"type": "blackeyed",
-	"owner": "deck",
-	"gold_I": 2,
-	"gold_II": 4,
-	"gold_III": 5,
-	"gold_IV": 6
-};
-
-beans.red = {
-	"type": "red",
-	"owner": "deck",
-	"gold_I": 2,
-	"gold_II": 3,
-	"gold_III": 4,
-	"gold_IV": 5
-};
-
-beans.garden = {
-	"type": "garden",
-	"owner": "deck",
-	"gold_I": 1000,
-	"gold_II": 2,
-	"gold_III": 3,
-	"gold_IV": 1000
-};
-
-beans.cocoa = {
-	"type": "garden",
-	"owner": "deck",
-	"gold_I": 1000,
-	"gold_II": 2,
-	"gold_III": 3,
-	"gold_IV": 4
-};
-
-module.exports = beans;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function callback () {
+// 	console.log('opened db successfully');
+// 	var Schema = mongoose.Schema;
+//
+// 	var beanSchema = new Schema({
+// 		type: String,
+// 		owner: String,
+// 		gold_I: Number,
+// 		gold_II: Number,
+// 		gold_III: Number,
+// 		gold_IV: Number
+// 	});
+//
+// 	var Bean = mongoose.model('Bean', beanSchema);
+//
+// 	var wax = new Bean({
+// 		"type": "wax",
+// 		"owner": "deck",
+// 		"gold_I": 4,
+// 		"gold_II": 7,
+// 		"gold_III": 9,
+// 		"gold_IV": 11
+// 	});
+//
+//
+// 	wax.save(function (err, wax) {
+// 		if (err) return console.error(err);
+// 		console.log(coffee.type);
+// 	});
+//
+//
+//
+//
+//
+//
+//
+// });
+//
+// // var search = db.beans.find();
+// // console.log(db);
+//
+// // console.log(search);
+//
+//
+//
+//
+//
+// var beans = {};
+//
+// beans.beanSchema = new Schema({
+// 	type: String,
+// 	owner: String,
+// 	gold_I: Number,
+// 	gold_II: Number,
+// 	gold_III: Number,
+// 	gold_IV: Number
+// });
+//
+// beans.protobean = mongoose.model('Bean', beans.beanSchema);
+//
+//
+// beans.coffee = {
+// 	"type": "coffee",
+// 	"owner": "deck",
+// 	"gold_I": 4,
+// 	"gold_II": 7,
+// 	"gold_III": 10,
+// 	"gold_IV": 12
+// };
+//
+// var err = "failed to save";
+// var coffee = new beans.protobean(beans.coffee);
+// coffee.save(function (err, coffee) {
+// 	if (err) return console.error(err);
+// 	console.log(coffee._id());
+// });
+//
+// console.log("test");
+//
+// // for (var i; i < 5; i++) {
+// // 	new beans.protobean(beans.coffee);
+// // }
+// // console.log(coffee);
+//
+// beans.wax = {
+// 	"type": "wax",
+// 	"owner": "deck",
+// 	"gold_I": 4,
+// 	"gold_II": 7,
+// 	"gold_III": 9,
+// 	"gold_IV": 11
+// };
+//
+// beans.blue = {
+// 	"type": "blue",
+// 	"owner": "deck",
+// 	"gold_I": 4,
+// 	"gold_II": 6,
+// 	"gold_III": 8,
+// 	"gold_IV": 10
+// };
+//
+// beans.chili = {
+// 	"type": "chili",
+// 	"owner": "deck",
+// 	"gold_I": 3,
+// 	"gold_II": 6,
+// 	"gold_III": 8,
+// 	"gold_IV": 9
+// };
+//
+// beans.stink = {
+// 	"type": "stink",
+// 	"owner": "deck",
+// 	"gold_I": 3,
+// 	"gold_II": 5,
+// 	"gold_III": 7,
+// 	"gold_IV": 8
+// };
+//
+// beans.green = {
+// 	"type": "green",
+// 	"owner": "deck",
+// 	"gold_I": 3,
+// 	"gold_II": 5,
+// 	"gold_III": 6,
+// 	"gold_IV": 7
+// };
+//
+// beans.soy = {
+// 	"type": "soy",
+// 	"owner": "deck",
+// 	"gold_I": 2,
+// 	"gold_II": 4,
+// 	"gold_III": 6,
+// 	"gold_IV": 7
+// };
+//
+// beans.blackeyed = {
+// 	"type": "blackeyed",
+// 	"owner": "deck",
+// 	"gold_I": 2,
+// 	"gold_II": 4,
+// 	"gold_III": 5,
+// 	"gold_IV": 6
+// };
+//
+// beans.red = {
+// 	"type": "red",
+// 	"owner": "deck",
+// 	"gold_I": 2,
+// 	"gold_II": 3,
+// 	"gold_III": 4,
+// 	"gold_IV": 5
+// };
+//
+// beans.garden = {
+// 	"type": "garden",
+// 	"owner": "deck",
+// 	"gold_I": 1000,
+// 	"gold_II": 2,
+// 	"gold_III": 3,
+// 	"gold_IV": 1000
+// };
+//
+// beans.cocoa = {
+// 	"type": "garden",
+// 	"owner": "deck",
+// 	"gold_I": 1000,
+// 	"gold_II": 2,
+// 	"gold_III": 3,
+// 	"gold_IV": 4
+// };
+//
+// module.exports = beans;
--- test.js
+++ test.js
@@ -1,25 +1,45 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
 
-mongoose.connect('mongodb://127.0.0.1/mydb');
 
-var Schema = mongoose.schema;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
 
-var blogSchema = new Schema({
-  title:  String,
-  author: String,
-  body:   String,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs:  Number
-  }
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+
+  var silence = new Kitten({ name: 'Silence' });
+
+  console.log(silence.name);
+
+  silence.save(function (err, silence) {
+    if (err) return console.error(err);
+    console.log("I am alive.");
+  });
+
+  // kittySchema.methods.speak = function () {
+  //   var greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
+  //   console.log(greeting);
+  // };
+  //
+  // var Kitten = mongoose.model('Kitten', kittySchema);
+  //
+  // var fluffy = new Kitten({ name: 'fluffy' });
+  //
+  // fluffy.speak();
+  //
+  // fluffy.save(function (err, fluffy) {
+  //   if (err) return console.error(err);
+  //   fluffy.speak();
+  // });
+
+
+
+
+
+
 });
-
-
-var blog = mongoose.model('blog', blogSchema);
-
-
-console.log('This is a test. TEST');
-console.log('Please test me Harry, test me');
