c7e26a653f2ff12fde91308a0eef58136c75ecaa
Steven Wright <steven.wright.b@gmail.com>
2014-11-23 20:20:34 -0500

    Beans as a sub-doc to the Deck.

--- beans.js
+++ beans.js
@@ -1,220 +1,112 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bohnanza');
-var db = mongoose.connection;
+var beans = {};
 
+var Schema = mongoose.Schema;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-
-	var Schema = mongoose.Schema;
-
-	var beanSchema = new Schema({
-		type: String,
-		owner: String,
-		gold_I: Number,
-		gold_II: Number,
-		gold_III: Number,
-		gold_IV: Number
-	});
-
-	var Bean = mongoose.model('Bean', beanSchema);
-
-	var black = new Bean({
-		"type": "black",
-		"owner": "deck",
-		"gold_I": 4,
-		"gold_II": 7,
-		"gold_III": 9,
-		"gold_IV": 11
-	});
-
-
-	black.save(function (err, black) {
-		if (err) return console.error(err);
-		console.log(black.type);
-		console.log('your bean is saved!');
-	});
-
+beans.schema = new Schema({
+	type: String,
+	gold_I: Number,
+	gold_II: Number,
+	gold_III: Number,
+	gold_IV: Number
 });
 
+// This is essentialy a private constructor
+var model = mongoose.model('Bean', beans.schema);
 
+beans.new = function (type) {
+	// if type isn't a key in types this is gonna be ugly.
+	types = {
+		"black-eyed": {
+			"type": "blackeyed",
+			"gold_I": 2,
+			"gold_II": 4,
+			"gold_III": 5,
+			"gold_IV": 6
+		},
 
+	 "blue": {
+			"type": "blue",
+			"gold_I": 4,
+			"gold_II": 6,
+			"gold_III": 8,
+			"gold_IV": 10
+		},
 
+		"chili" : {
+			"type": "chili",
+			"gold_I": 3,
+			"gold_II": 6,
+			"gold_III": 8,
+			"gold_IV": 9
+		},
 
+		"cocoa" : {
+			"type": "garden",
+			"gold_I": 1000,
+			"gold_II": 2,
+			"gold_III": 3,
+			"gold_IV": 4
+		},
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback () {
-// 	console.log('opened db successfully');
-// 	var Schema = mongoose.Schema;
-//
-// 	var beanSchema = new Schema({
-// 		type: String,
-// 		owner: String,
-// 		gold_I: Number,
-// 		gold_II: Number,
-// 		gold_III: Number,
-// 		gold_IV: Number
-// 	});
-//
-// 	var Bean = mongoose.model('Bean', beanSchema);
-//
-// 	var wax = new Bean({
-// 		"type": "wax",
-// 		"owner": "deck",
-// 		"gold_I": 4,
-// 		"gold_II": 7,
-// 		"gold_III": 9,
-// 		"gold_IV": 11
-// 	});
-//
-//
-// 	wax.save(function (err, wax) {
-// 		if (err) return console.error(err);
-// 		console.log(coffee.type);
-// 	});
-//
-//
-//
-//
-//
-//
-//
-// });
-//
-// // var search = db.beans.find();
-// // console.log(db);
-//
-// // console.log(search);
-//
-//
-//
-//
-//
-// var beans = {};
-//
-// beans.beanSchema = new Schema({
-// 	type: String,
-// 	owner: String,
-// 	gold_I: Number,
-// 	gold_II: Number,
-// 	gold_III: Number,
-// 	gold_IV: Number
-// });
-//
-// beans.protobean = mongoose.model('Bean', beans.beanSchema);
-//
-//
-// beans.coffee = {
-// 	"type": "coffee",
-// 	"owner": "deck",
-// 	"gold_I": 4,
-// 	"gold_II": 7,
-// 	"gold_III": 10,
-// 	"gold_IV": 12
-// };
-//
-// var err = "failed to save";
-// var coffee = new beans.protobean(beans.coffee);
-// coffee.save(function (err, coffee) {
-// 	if (err) return console.error(err);
-// 	console.log(coffee._id());
-// });
-//
-// console.log("test");
-//
-// // for (var i; i < 5; i++) {
-// // 	new beans.protobean(beans.coffee);
-// // }
-// // console.log(coffee);
-//
-// beans.wax = {
-// 	"type": "wax",
-// 	"owner": "deck",
-// 	"gold_I": 4,
-// 	"gold_II": 7,
-// 	"gold_III": 9,
-// 	"gold_IV": 11
-// };
-//
-// beans.blue = {
-// 	"type": "blue",
-// 	"owner": "deck",
-// 	"gold_I": 4,
-// 	"gold_II": 6,
-// 	"gold_III": 8,
-// 	"gold_IV": 10
-// };
-//
-// beans.chili = {
-// 	"type": "chili",
-// 	"owner": "deck",
-// 	"gold_I": 3,
-// 	"gold_II": 6,
-// 	"gold_III": 8,
-// 	"gold_IV": 9
-// };
-//
-// beans.stink = {
-// 	"type": "stink",
-// 	"owner": "deck",
-// 	"gold_I": 3,
-// 	"gold_II": 5,
-// 	"gold_III": 7,
-// 	"gold_IV": 8
-// };
-//
-// beans.green = {
-// 	"type": "green",
-// 	"owner": "deck",
-// 	"gold_I": 3,
-// 	"gold_II": 5,
-// 	"gold_III": 6,
-// 	"gold_IV": 7
-// };
-//
-// beans.soy = {
-// 	"type": "soy",
-// 	"owner": "deck",
-// 	"gold_I": 2,
-// 	"gold_II": 4,
-// 	"gold_III": 6,
-// 	"gold_IV": 7
-// };
-//
-// beans.blackeyed = {
-// 	"type": "blackeyed",
-// 	"owner": "deck",
-// 	"gold_I": 2,
-// 	"gold_II": 4,
-// 	"gold_III": 5,
-// 	"gold_IV": 6
-// };
-//
-// beans.red = {
-// 	"type": "red",
-// 	"owner": "deck",
-// 	"gold_I": 2,
-// 	"gold_II": 3,
-// 	"gold_III": 4,
-// 	"gold_IV": 5
-// };
-//
-// beans.garden = {
-// 	"type": "garden",
-// 	"owner": "deck",
-// 	"gold_I": 1000,
-// 	"gold_II": 2,
-// 	"gold_III": 3,
-// 	"gold_IV": 1000
-// };
-//
-// beans.cocoa = {
-// 	"type": "garden",
-// 	"owner": "deck",
-// 	"gold_I": 1000,
-// 	"gold_II": 2,
-// 	"gold_III": 3,
-// 	"gold_IV": 4
-// };
-//
-// module.exports = beans;
+		"coffee" : {
+			"type": "coffee",
+			"gold_I": 4,
+			"gold_II": 7,
+			"gold_III": 10,
+			"gold_IV": 12
+		},
+
+		"garden" : {
+			"type": "garden",
+			"gold_I": 1000,
+			"gold_II": 2,
+			"gold_III": 3,
+			"gold_IV": 1000
+		},
+
+		"green" : {
+			"type": "green",
+			"gold_I": 3,
+			"gold_II": 5,
+			"gold_III": 6,
+			"gold_IV": 7
+		},
+
+		"soy" : {
+			"type": "soy",
+			"gold_I": 2,
+			"gold_II": 4,
+			"gold_III": 6,
+			"gold_IV": 7
+		},
+
+		"stink" : {
+			"type": "stink",
+			"gold_I": 3,
+			"gold_II": 5,
+			"gold_III": 7,
+			"gold_IV": 8
+		},
+
+		"red" : {
+			"type": "red",
+			"gold_I": 2,
+			"gold_II": 3,
+			"gold_III": 4,
+			"gold_IV": 5
+		},
+
+		"wax" : {
+			"type": "wax",
+			"gold_I": 4,
+			"gold_II": 7,
+			"gold_III": 9,
+			"gold_IV": 11
+		}
+	};
+
+	return new model(types[type]);
+}
+
+module.exports = beans;
--- deck.js
+++ deck.js
@@ -1,49 +1,50 @@
 var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-
 var beans = require('./beans');
-
 var deck = {};
-
+var Schema = mongoose.Schema;
 deck.deckSchema = new Schema({
 	channel: String,
-	cards: Array
+	cards: [beans.schema]
 });
 
-deck.new_deck = mongoose.model('Deck', this.deckSchema);
-
-deck.totals = {
-	"coffee": 24,
-	"wax": 22,
-	"blue": 20,
-	"chili": 18,
-	"stink": 16,
-	"green": 14,
-	"soy": 12,
-	"blackeyed": 10,
-	"red": 8,
-	"garden": 6,
-	"cocoa": 4
-};
-
-
-console.log(this.totals);
-
+var model = mongoose.model('Deck', deck.deckSchema);
+//do we want camel case, underscores, or just concat??
 deck.makedeck = function() {
-	var new_deck = new Deck('default_channel', []);
-	for (var bohn in this.totals) {
-		// if (typeof bohn != 'undefined' || typeof bohn != 'function') {
-			for (var i = 0; i < this[bohn]; i++) {
-				var new_card = new Bean(beans[bohn]);
-				new_deck.push(new_card);
-			}
-		// }
+	totals = {
+		"coffee": 24,
+		"wax": 22,
+		"blue": 20,
+		"chili": 18,
+		"stink": 16,
+		"green": 14,
+		"soy": 12,
+		"black-eyed": 10,
+		"red": 8,
+		"garden": 6,
+		"cocoa": 4
+	};
+	var new_deck = new deck.model({channel: 'default_channel', cards: []});
+	for (var bohn in deck.totals) {
+		for (var i=0; i < deck.totals[bohn]; i++) {
+			var new_card = beans.new(bohn);
+			new_deck.cards.push(new_card);
+		}
 	}
-	return this;
-	// return new_deck;
+	//Shuffle?
+	return new_deck;
 };
 
-console.log(deck.makedeck());
-
+//I'm not sure this code should live here, but this is demonstrative
+mongoose.connect('mongodb://localhost/test');
+//mongoose.connect('mongodb://127.0.0.1:27017/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("Generating new deck:");
+	var new_deck = deck.makedeck();
+	//ignoring error handling
+	new_deck.save();
+	console.log(new_deck);
+	console.log("Saved new deck.");
+});
 module.exports = deck;
