e2165202fc9d48dfa228c7567af3614e0fea37b6
jdmcpeek <david.mcpeek@yale.edu>
2014-11-26 01:59:29 -0500

    first step into event emitters and receivers

--- app.js
+++ app.js
@@ -5,14 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
 
-var app = express();
+var express = require('express.io');
+var app = express().http().io();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+// Broadcast the new visitor event on ready route.
+app.io.route('check', function(req) {
+  req.io.broadcast('new visitor');
+});
+
+// Send client html.
+app.get('/', function(req, res) {
+  res.render('play');
+});
+
+app.listen(7076, function(err) {
+  if (err) {
+    console.log("failed to connect");
+  }
+  console.log("listening on port 7076");
+});
+
 
 // uncomment after placing your favicon in /pubdc
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +42,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
