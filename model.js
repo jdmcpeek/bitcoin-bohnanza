7a4fc13d710cea81d44d7515f6376353655b72c4
Steven Wright <steven.wright.b@gmail.com>
2014-11-26 18:35:13 -0500

    Created model.js, previously known as mongoosing.js, to handle requests from controller to DB.

---
+++ model.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var bean_model = require('./bean');
+var plot_model = require('./plot');
+var player_model = require('./player');
+var game_model = require('./game');
+
+mongoose.connect('mongodb://localhost/test');
+db = mongoose.connection;
+db.on('open', function callback() {
+
+  exports.create_game = function(channel_name, player_name) {
+    var new_player = player_model.create({name: player_name});
+    var new_game = game_model.create({channel: channel_name, players: [new_player]});
+    console.log("create_game");
+    return new_game;
+  };
+
+  exports.save_game = function(game, cb) {
+    game.save(function(err, game){
+      if(err) console.error(err);
+      else cb(game);
+    });
+    console.log("Saved!");
+  };
+
+  exports.find_game = function(channel_name, cb) {
+    console.log("find_game");
+    game_model.findOne({channel: channel_name},
+      function (err, game) {
+      if(err) console.error(err);
+        cb(game);
+    });
+  };
+});
+db.on('error', console.error.bind(console, 'connection error:'));
