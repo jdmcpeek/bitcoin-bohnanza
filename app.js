94941d064bc98aaa766b1ff07fc7528c3e00b3ae
Steven Wright <steven.wright.b@gmail.com>
2014-11-27 03:25:13 -0500

    this is a really lame commit. Trying to extract channel param

--- app.js
+++ app.js
@@ -1,102 +1,103 @@
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Database Stuffz
 var mongoose = require('mongoose');
 var model = require('./model');
+db = mongoose.connection;
+mongoose.connect('mongodb://localhost/test');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', function() {console.log('connected to database');});
 
 var express = require('express.io');
 var app = express().http().io();
-
-db = mongoose.connection;
-mongoose.connect('mongodb://localhost/test');
-  console.log('connected bitches');
-
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-  // // Broadcast the new visitor event on ready route.
-  // app.io.route('check', function(req){
-  //   app.io.broadcast('key', model.create_game("SAMP", "LE"));
-  // });
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-  app.io.route('start', function(req){
-    app.io.broadcast('hahaha');
+// Traditional Routing
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/generate', function(req, res){
+  //console.log(req.body);
+  var game = model.create_game(req.body.channel_name, req.body.player_name);
+  // we will have validation issues to handle.
+  model.save_game(game);
+  res.redirect(301, "/play/"+req.body.channel_name);
+});
+
+app.get('/play/:channel', function(req, res){
+  var game;
+  model.find_game(req.param('channel'), function(err, found) {
+    if(err) console.error(err);
+    game = found;
   });
+  console.log(game.channel);
+  res.render('play', {channel: game.channel});
+});
+// Socket Routing
+app.io.route('start', function(req){
+  app.io.broadcast('hahaha');
+});
 
-
-  app.io.route('index', function(req) {
-    var game = model.find_game('Test', function(bum) {
-      bum.round = bum.round + 1;
-      model.save_game(bum);
-    });
+app.io.route('index', function(req) {
+  var game = model.find_game('Test', function(bum) {
+    bum.round = bum.round + 1;
+    model.save_game(bum);
   });
+});
 
-  // Send client html.
-  app.get('/', function(req, res) {
-    res.render('index');
-  });
-
-  app.post('/generate', function(req, res) {
-    //console.log(req);
-    res.render('index');
-    // res.send(req);
-  });
-
-  app.listen(7076, function(err) {
-    if (err) {
-      console.log("failed to connect");
-    }
-    console.log("listening on port 7076");
-  });
-
-
-  // uncomment after placing your favicon in /pubdc
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  //app.use(bodyParser());
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  // app.use('/', routes);
-  // app.use('/users', users);
-
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  // error handlers
-
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: err
-      });
-    });
+// Listen!
+app.listen(7076, function(err) {
+  if (err) {
+    console.log("failed to connect");
   }
+  console.log("listening on port 7076");
+});
 
-  // production error handler
-  // no stacktraces leaked to user
+//Uncomment after placing your favicon in /pubdc
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: {}
+      error: err
     });
   });
+}
 
-//db.on('error', console.error.bind(console, 'connection error:'));
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 module.exports = app;
