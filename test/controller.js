ffcb5c550297560b4eb6175c5ff406682e80566a
jdmcpeek <david.mcpeek@yale.edu>
2014-12-04 22:12:28 -0500

    test ng Directives when those get written

--- test/controller.js
+++ test/controller.js
@@ -25,7 +25,10 @@ describe("controller", function(){
 
   });
 
-
+  // The beauty of Angular is that our controller will allow the player making changes to see
+  // immediate effects on the screen; the controller will update immediately upon DOM events;
+  // the only lag will happen for other players, and still they'll receive all updates at once,
+  // so it will be unnoticeable.
   describe("#update", function(){
 
     it("should reflect both the database and the DOM at all times, exluding exceptions", function(){
@@ -41,8 +44,12 @@ describe("controller", function(){
     });
 
     describe("#receive", function(){
+      // add functions to $scope to response to directives... these functions should io.emit to the server.
+      it("should update $scope upon ng Directive events, then io.emit these changes to the server", function(){
 
-      it("should update upon receiving an update event", function(){
+      });
+
+      it("should update upon receiving an update event from the server", function(){
 
       });
 
